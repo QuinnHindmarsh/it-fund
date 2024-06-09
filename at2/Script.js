15352939afa6e118172c44543ae5e8761cfcb2bb
TheToastGuy <hinqc001@mymail.unisa.edu.au>
2024-06-10 01:45:57 +0930

    theme goes off local storage

--- at2/Script.js
+++ at2/Script.js
@@ -3,9 +3,9 @@ function ChangeForward() /*changes the image and text to the emblem*/
 {
     var img = document.getElementById("Img");
     var txt = document.getElementById('ImgText')
-    img.src="images/UNISA_Emblem2.png";
-    img.alt='UNISA emblem';
-    txt.innerText='attributions for UNISA emblem';
+    img.src="images/UNISACampusPhoto.jpg";
+    img.alt='Photo of UNISA campus - but a different one';
+    txt.innerText='File:UNISA Main Campus.jpg https://t.ly/lWrch';
 }
 
 
@@ -14,17 +14,52 @@ function ChangeBackward()/* changes the image and the text to a cmapus photo*/
 {
     var img = document.getElementById("Img");
     var txt = document.getElementById('ImgText')
-    img.src="images/MawsonLakesImg2.jpg";
-    img.alt='Attributions for Mawson lakes campus photo'
-    txt.innerText='attributions for UNISA Campus';
+    img.src="images/UNISACampus2.jpg";
+    img.alt='Photo of UNISA campus'
+    txt.innerText='File:UniSA Library, Adelaide, East view 20230207.jpg - Wikimedia Commons https://shorturl.at/jka10';
 }
 
 function ChangeTheme()/*toggles between themes*/
 {
-    /*const theme_toggle = document.getElementById('theme_toggle');*/
+
     var data_theme = document.documentElement.getAttribute("data-theme");
     document.documentElement.setAttribute("data-theme", data_theme === "light" ? "dark" : "light")
+
+    localStorage.setItem("Theme", data_theme === "light" ? "dark" : "light");
+    onload()
 
 
 }
 
+
+function UserName(){
+    const txt = document.getElementById('HeadText');
+
+
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const userName = urlParams.get('user_name');
+
+    if (userName == null || userName == ""){
+        txt.innerText='My first year at UNISA';
+    }
+    else {
+        txt.innerText= "Hello, " + userName;
+    }
+
+}
+
+function OnLoad(){
+    var data_theme = document.documentElement.getAttribute("data-theme");
+
+    const theme = localStorage.getItem("Theme");
+
+    console.log(theme);
+    document.documentElement.setAttribute("data-theme",  theme)
+
+
+
+}
+
+/*window.onload = function() {UserName()} */
+window.onload = OnLoad;
